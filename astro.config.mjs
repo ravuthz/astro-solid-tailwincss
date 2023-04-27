7db9d232b22e20ce7dbd91195cd370cd159f1821
Ravuthz <ravuthz@gmail.com>
2023-04-27 21:31:06 +0700

    Config env, sitemap, markdown and alias path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
@@ -7,8 +7,15 @@ import image from "@astrojs/image";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
+const { VITE_APP_BASE_URL } = import.meta.env;
+
+console.log("VITE_APP_BASE_URL: ", VITE_APP_BASE_URL);
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-solid-tailwincss.com",
-  integrations: [mdx(), sitemap(), partytown(), image(), solidJs(), tailwind()]
+  site: VITE_APP_BASE_URL,
+  integrations: [mdx(), sitemap(), partytown(), image(), solidJs(), tailwind()],
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: { theme: "dracula" },
+  },
 });
