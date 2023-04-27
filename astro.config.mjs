530f62a37f0735d2d9b54080c445d657a4fa530d
Ravuthz <ravuthz@gmail.com>
2023-04-27 21:05:49 +0700

    Add mdx sitemap partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astro-solid-tailwincss.com",
+  integrations: [mdx(), sitemap(), partytown()]
+});
