7b0efeab3c82a97d2ee02ff3eb1230f19484b376
Ravuthz <ravuthz@gmail.com>
2023-04-27 21:07:07 +0700

    Add image solid tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
+import image from "@astrojs/image";
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-solid-tailwincss.com",
-  integrations: [mdx(), sitemap(), partytown()]
+  integrations: [mdx(), sitemap(), partytown(), image(), solidJs(), tailwind()]
 });
